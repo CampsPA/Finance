312730c843379f2dea22b629ed9d40abea6fa1c0
Paul Campos <campos.paul@outlook.com>
2024-08-06 19:04:47 -0400

    Update excel_statements.py

--- excel_statements.py
+++ excel_statements.py
@@ -13,6 +13,7 @@ class Analysis:
         self.cash_flow = pd.DataFrame()
         self.cash_flow = pd.DataFrame()
         self.assumptions = pd.DataFrame()
+        self.years =  ['Year-1', 'Year-2', 'Year-3', 'Year-4', 'Year-5']
         
 
     def get_statements_data(self):
@@ -23,7 +24,6 @@ class Analysis:
 
     def statements(self):
         self.assumptions = pd.read_excel(self.data, sheet_name = 'Assumptions', index_col= 'Metrics')
-        #self.assumptions = self.assumptions[['Year2','Year1','Year0']]
         self.income_statement = pd.read_excel(self.data, sheet_name='Income Statement', index_col='Income Statement')
         self.balance_sheet = pd.read_excel(self.data, sheet_name='Balance Sheet', index_col='Balance Sheet')
         self.cash_flow =  pd.read_excel(self.data, sheet_name='Cash Flows', index_col='Cash Flows')
@@ -32,14 +32,13 @@ class Analysis:
 
 
     def populated_assumptions_historical(self):
-        
         # Helper method to format values
         def format_percentage(value):
             return "{:.1%}".format(value)
 
         def format_fixed_cost(value):
             try:
-                return "{:.0f}".format(float(value))
+                return "{:.2f}".format(float(value))
             except ValueError:
                 return "N/A"
 
@@ -106,19 +105,103 @@ class Analysis:
         self.inc_tax_pay_0 = format_percentage(self.balance_sheet.at['Income Taxes Payable', 'Year0'] / self.income_statement.at['Taxes', 'Year0'] * -1)
 
         # Long Term Debt
-        self.long_term_debt_2 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year2'] = self.balance_sheet.at['Long-Term Debt', 'Year2'])
-        self.long_term_debt_1 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year1'] = self.balance_sheet.at['Long-Term Debt', 'Year1'])
-        self.long_term_debt_0 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year0'] = self.balance_sheet.at['Long-Term Debt', 'Year0']) 
+        self.assumptions.at['Long Term Debt', 'Year2'] = self.balance_sheet.at['Long-Term Debt', 'Year2']
+        self.long_term_debt_2 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year2'])
+
+        self.assumptions.at['Long Term Debt', 'Year1'] = self.balance_sheet.at['Long-Term Debt', 'Year1']
+        self.long_term_debt_1 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year1'])
+
+        self.assumptions.at['Long Term Debt', 'Year0'] = self.balance_sheet.at['Long-Term Debt', 'Year0']
+        self.long_term_debt_0 = format_fixed_cost(self.assumptions.at['Long Term Debt', 'Year0'])
 
         #Common Share Capital
+        self.assumptions.at['Common Share Capital', 'Year2'] = self.balance_sheet.at['Common Stock and Additional Paid-In Capital', 'Year2']
+        self.common_share_cap_2 = format_fixed_cost(self.assumptions.at['Common Share Capital', 'Year2'])
+
+        self.assumptions.at['Common Share Capital', 'Year1'] = self.balance_sheet.at['Common Stock and Additional Paid-In Capital', 'Year1']
+        self.common_share_cap_1 = format_fixed_cost(self.assumptions.at['Common Share Capital', 'Year1'])
+
+        self.assumptions.at['Common Share Capital', 'Year0'] = self.balance_sheet.at['Common Stock and Additional Paid-In Capital', 'Year0']
+        self.common_share_cap_0 = format_fixed_cost(self.assumptions.at['Common Share Capital', 'Year0'])
+
+         #Dividend Payout Ratio
+        self.div_payout_ratio_2 =  format_percentage(self.income_statement.at['Common Dividends', 'Year2'] / self.income_statement.at['Net Income', 'Year2'])
+        self.div_payout_ratio_1 =  format_percentage(self.income_statement.at['Common Dividends', 'Year1'] / self.income_statement.at['Net Income', 'Year1'])
+        self.div_payout_ratio_0 =  format_percentage(self.income_statement.at['Common Dividends', 'Year0'] / self.income_statement.at['Net Income', 'Year0'])
 
 
-
-
+    
     # Define the function to assign a value
     def assign_values(self, df, values_dict):
         for (row_label, column_label), value in values_dict.items():
             df.at[row_label, column_label] = value
+
+   ########################################################################################### 
+
+    
+    def populate_assumptions_future(self):
+        metrics = ['Capital Asset Turnover Ratio                           (x)',
+           'Receivable Days (Sales Basis)                     (Days)',
+           'Inventory Days (COGS Basis)                       (Days)',
+           'Payable Days (COGS Basis)                          (Days)']
+        '''
+        metrics = ['Sales Growth',
+           'Gross Margin', 
+           'Distribution Expense (Percent of Sales)', 
+           'Marketing & Admin Expense (Fixed Cost)',
+           'Research Expense (Percent of Sales)',
+           'Depreciation (Percent of Sales)', 
+           'Long-Term Debt Interest Rate (Average Debt)',
+           'Tax Rate (Percent of EBT)',
+           'Capital Asset Turnover Ratio (x)',
+           'Receivable Days (Sales Basis) (Days)',
+           'Inventory Days (COGS Basis) (Days)',
+           'Payable Days (COGS Basis) (Days)',
+           'Income Tax Payable (Percent of Taxes) (Days)',
+           'Long Term Debt',
+           'Common Share Capital',
+            'Dividend Payout Ratio'
+          ]
+          '''
+        
+         # Function to get user input
+        def get_user_input(prompt):
+            while True:
+                try:
+                    value = float(input(prompt))
+                    return value
+                except ValueError:
+                    print("Invalid input. Please enter a numerical value.")
+
+        # Function to format values based on type
+        def format_value(value, is_percentage=False):
+            if is_percentage:
+                return "{:.1%}".format(value)
+            else:
+                return "{:.2f}".format(value)  # For raw numbers with no decimal places
+    
+
+        # Get user inputs for each metric and year
+        for metric in metrics:
+            if metric in metrics:
+                is_percentage = True
+            else:
+                is_percentage = False
+
+            same_value = input(f"Do you want to enter the same value for all years for {metric}? (yes/no): ").strip().lower()
+            
+            if same_value == 'yes':
+                value = get_user_input(f"Enter {metric} (as a decimal, e.g., 0.06 for 6%): ")
+                formatted_value = format_value(value, is_percentage)
+                self.assumptions.loc[metric, :] = formatted_value
+            else:
+                for year in self.years:
+                    prompt = f"Enter {metric} for {year} (as a decimal, e.g., 0.06 for 6%): " if is_percentage else f"Enter {metric} for {year}: "
+                    value = get_user_input(prompt)
+                    self.assumptions.at[metric, year] = format_value(value, is_percentage)
+
+
+###############################################################################################
 
 
 # Run the program
@@ -127,9 +210,10 @@ if __name__ == '__main__':
     analysis.get_statements_data()
     analysis.statements()
     analysis.populated_assumptions_historical()
+    analysis.populate_assumptions_future()
     
-
-        
+    
+   
     
 
    
@@ -163,30 +247,33 @@ values_dict = {
           ('Receivable Days (Sales Basis)                     (Days)', 'Year2'): analysis.receivables_days_2,
           ('Receivable Days (Sales Basis)                     (Days)', 'Year1'): analysis.receivables_days_1,
           ('Receivable Days (Sales Basis)                     (Days)', 'Year0'): analysis.receivables_days_0,
-          ('Inventory Days (COGS Basis)                       (Days)', 'Year-2'): analysis.inv_days_2,
-          ('Inventory Days (COGS Basis)                       (Days)', 'Year-1'): analysis.inv_days_1,
-          ('Inventory Days (COGS Basis)                       (Days)', 'Year-0'): analysis.inv_days_0,
-          ('Payable Days (COGS Basis)                          (Days)', 'Year-2'): analysis.payable_days_2,
-          ('Payable Days (COGS Basis)                          (Days)', 'Year-1'): analysis.payable_days_1,
-          ('Payable Days (COGS Basis)                          (Days)', 'Year-0'): analysis.payable_days_0,
-          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year-2'): analysis.inc_tax_pay_2,
-          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year-1'): analysis.inc_tax_pay_1,
-          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year-0'): analysis.inc_tax_pay_0,
-          ('Long Term Debt', 'Year-2'): analysis.long_term_debt_2,
-          ('Long Term Debt', 'Year-1'): analysis.long_term_debt_1,
-          ('Long Term Debt', 'Year-0'): analysis.long_term_debt_0,
-        #  ('Common Share Capital', 'Year-2'): analysis.common_share_cap_2,
-        #  ('Common Share Capital', 'Year-1'): analysis.common_share_cap_1,
-        #  ('Common Share Capital', 'Year-0'): analysis.common_share_cap_0,
-        #  ('Dividend Payout Ratio ', 'Year-2'): analysis.div_payout_ratio_2,
-        #  ('Dividend Payout Ratio ', 'Year-1'): analysis.div_payout_ratio_1,
-        #  ('Dividend Payout Ratio ', 'Year-0'): analysis.div_payout_ratio_0,   
+          ('Inventory Days (COGS Basis)                       (Days)', 'Year2'): analysis.inv_days_2,
+          ('Inventory Days (COGS Basis)                       (Days)', 'Year1'): analysis.inv_days_1,
+          ('Inventory Days (COGS Basis)                       (Days)', 'Year0'): analysis.inv_days_0,
+          ('Payable Days (COGS Basis)                          (Days)', 'Year2'): analysis.payable_days_2,
+          ('Payable Days (COGS Basis)                          (Days)', 'Year1'): analysis.payable_days_1,
+          ('Payable Days (COGS Basis)                          (Days)', 'Year0'): analysis.payable_days_0,
+          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year2'): analysis.inc_tax_pay_2,
+          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year1'): analysis.inc_tax_pay_1,
+          ('Income Tax Payable (Percent of Taxes) (Days)', 'Year0'): analysis.inc_tax_pay_0,
+          ('Long Term Debt', 'Year2'): analysis.long_term_debt_2,
+          ('Long Term Debt', 'Year1'): analysis.long_term_debt_1,
+          ('Long Term Debt', 'Year0'): analysis.long_term_debt_0,
+          ('Common Share Capital', 'Year2'): analysis.common_share_cap_2,
+          ('Common Share Capital', 'Year1'): analysis.common_share_cap_1,
+          ('Common Share Capital', 'Year0'): analysis.common_share_cap_0,
+          ('Dividend Payout Ratio ', 'Year2'): analysis.div_payout_ratio_2,
+          ('Dividend Payout Ratio ', 'Year1'): analysis.div_payout_ratio_1,
+          ('Dividend Payout Ratio ', 'Year0'): analysis.div_payout_ratio_0,   
      }
-  
-
-
 
 analysis.assign_values(analysis.assumptions, values_dict)
+
+
+
+
+
+
 
 
 
